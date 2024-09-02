daa77d4ff67446a2ab0ef91653ea12a0119ca96d
mtntuan107 <mtntuan.107@gmail.com>
2024-09-02 20:02:15 +0700

    create foodmanagent component

--- pokeapp/src/App.js
+++ pokeapp/src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import Home from "./components/Home";
-
+import FoodManagement from "./components/FoodManagement";
 function App() {
   return (
-  <>
-    <BrowserRouter>
-      <Header />
-      <Footer />
-    </BrowserRouter>
-  </>
+  // <>
+  //   <BrowserRouter>
+  //     <Header />
+  //     <Footer />
+    
+  //   </BrowserRouter>
+  // </>
+  <FoodManagement/>
   )
 }
 
